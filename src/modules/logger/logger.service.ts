2f314548c87efeaebffe932bbe78c896fa3c2eed
nikita <xiagn17@gmail.com>
2021-10-08 20:59:05 +0300

    logger small change

--- src/modules/logger/logger.service.ts
+++ src/modules/logger/logger.service.ts
@@ -3,13 +3,14 @@ import { ConsoleLogger, Injectable, Scope } from '@nestjs/common';
 @Injectable({ scope: Scope.TRANSIENT })
 export class Logger extends ConsoleLogger {
   warn(message: string): void {
-    super.log(message);
-    // eslint-disable-next-line no-console
-    console.log(message);
+    super.warn(message);
   }
 
-  error(message: string, trace?: string): void {
-    super.error(message, trace);
-    console.error(message, trace);
+  log(message: string, ...other: any[]): void {
+    super.log(message, ...other);
+  }
+
+  error(message: string): void {
+    super.error(message);
   }
 }
