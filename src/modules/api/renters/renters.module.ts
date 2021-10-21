ec598921f9b4d265a0f6fc76ec2f467b39102b43
nikita <xiagn17@gmail.com>
2021-10-21 19:17:29 +0300

    UPDATE: add analytics table

--- src/modules/api/renters/renters.module.ts
+++ src/modules/api/renters/renters.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule } from '../../logger/logger.module';
+import { AnalyticsModule } from '../analytics/analytics.module';
 import { RentersService } from './renters.service';
 import { RentersSerializer } from './renters.serializer';
 import { RentersController } from './renters.controller';
 
 @Module({
-  imports: [LoggerModule],
+  imports: [LoggerModule, AnalyticsModule],
   controllers: [RentersController],
   providers: [RentersService, RentersSerializer],
   exports: [RentersService, RentersSerializer],
