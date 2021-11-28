686ce8a266704e5b76a63ef1c91b81df97f4bd1e
xiagn17 <xiagn17@gmail.com>
2021-11-28 19:36:42 +0300

    IMPROVE: 1) added String() parsing to configuration. 2) added script to package.json to compile project with webstorm debugger

--- src/modules/configuration/configuration.ts
+++ src/modules/configuration/configuration.ts
@@ -6,7 +6,7 @@ const GOOGLE_SHEETS_CONFIG_FILENAME = 'google-sheets.json';
 
 export default (): ConfigurationType => {
   const googleSheetConfigJson = readFileSync(
-    join(__dirname, '..', '..', '..', GOOGLE_SHEETS_CONFIG_FILENAME),
+    join(__dirname, '..', '..', GOOGLE_SHEETS_CONFIG_FILENAME),
     'utf8',
   );
   const googleSheetConfig: GoogleSheetsConfigType = JSON.parse(
@@ -17,21 +17,21 @@ export default (): ConfigurationType => {
     google_sheets: googleSheetConfig,
     port: Number(process.env.PORT) || 3000,
     database: {
-      db_username: process.env.POSTGRES_USERNAME,
-      db_password: process.env.POSTGRES_PASSWORD,
-      db_database: process.env.POSTGRES_DATABASE,
-      host: process.env.POSTGRES_HOST,
-      port: parseInt(process.env.POSTGRES_PORT, 10),
+      db_username: String(process.env.POSTGRES_USERNAME),
+      db_password: String(process.env.POSTGRES_PASSWORD),
+      db_database: String(process.env.POSTGRES_DATABASE),
+      host: String(process.env.POSTGRES_HOST),
+      port: parseInt(String(process.env.POSTGRES_PORT), 10),
       settings: {
         synchronize: process.env.TYPEORM_SYNCHRONIZE === 'true',
         logging: process.env.TYPEORM_LOGGING === 'true',
         migrationsRun: process.env.TYPEORM_MIGRATIONS_RUN === 'true',
       },
     },
-    telegramToken: process.env.TELEGRAM_TOKEN,
+    telegramToken: String(process.env.TELEGRAM_TOKEN),
     sendpulse: {
-      clientId: process.env.SENDPULSE_CLIENT_ID,
-      clientSecret: process.env.SENDPULSE_CLIENT_SECRET,
+      clientId: String(process.env.SENDPULSE_CLIENT_ID),
+      clientSecret: String(process.env.SENDPULSE_CLIENT_SECRET),
     },
     renterMatches: {
       trialMatchesCount: Number(process.env.TRIAL_FREE_MATCHES_COUNT),
