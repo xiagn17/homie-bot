0ae2cadc890e4ea31f0602562fd89a89d614e80d
xiagn17 <xiagn17@gmail.com>
2022-01-08 21:00:52 +0300

    FIX: remove google-sheets.json and docker-compose.yml fix

--- src/modules/configuration/configuration.ts
+++ src/modules/configuration/configuration.ts
@@ -1,20 +1,7 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-import { ConfigurationType, GoogleSheetsConfigType } from './configuration.types';
-
-const GOOGLE_SHEETS_CONFIG_FILENAME = 'google-sheets.json';
+import { ConfigurationType } from './configuration.types';
 
 export default (): ConfigurationType => {
-  const googleSheetConfigJson = readFileSync(
-    join(__dirname, '..', '..', GOOGLE_SHEETS_CONFIG_FILENAME),
-    'utf8',
-  );
-  const googleSheetConfig: GoogleSheetsConfigType = JSON.parse(
-    googleSheetConfigJson,
-  ) as GoogleSheetsConfigType;
-
   return {
-    google_sheets: googleSheetConfig,
     port: Number(process.env.PORT) || 3000,
     database: {
       db_username: String(process.env.POSTGRES_USERNAME),
--- src/modules/configuration/configuration.types.ts
+++ src/modules/configuration/configuration.types.ts
@@ -1,15 +1,3 @@
-export interface GoogleSheetsConfigType {
-  type: string;
-  project_id: string;
-  private_key_id: string;
-  private_key: string;
-  client_email: string;
-  client_id: string;
-  auth_uri: string;
-  token_uri: string;
-  auth_provider_x509_cert_url: string;
-  client_x509_cert_url: string;
-}
 export interface DatabaseConfigType {
   db_username: string;
   db_password: string;
@@ -35,7 +23,6 @@ export interface RedisConfigType {
 
 export interface ConfigurationType {
   port: number;
-  google_sheets: GoogleSheetsConfigType;
   database: DatabaseConfigType;
   redis: RedisConfigType;
   telegramToken: string;
