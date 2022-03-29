c8b7aa7492be5b3894062b7a8b2296ce0ea826ac
xiagn17 <xiagn17@gmail.com>
2022-03-30 00:03:02 +0300

    FIX: import module

--- src/modules/api/landlord-objects/landlord-objects.module.ts
+++ src/modules/api/landlord-objects/landlord-objects.module.ts
@@ -2,12 +2,13 @@ import { forwardRef, Module } from '@nestjs/common';
 import { LoggerModule } from '../../logger/logger.module';
 import { LandlordRenterMatchesModule } from '../landlord-renter-matches/landlord-renter-matches.module';
 import { TasksSchedulerModule } from '../../tasks/scheduler/tasks.scheduler.module';
+import { RentersModule } from '../renters/renters.module';
 import { LandlordObjectsService } from './landlord-objects.service';
 import { LandlordObjectsSerializer } from './landlord-objects.serializer';
 import { LandlordObjectsControlService } from './landlord-objects.control.service';
 
 @Module({
-  imports: [LoggerModule, TasksSchedulerModule, forwardRef(() => LandlordRenterMatchesModule)],
+  imports: [LoggerModule, TasksSchedulerModule, forwardRef(() => LandlordRenterMatchesModule), RentersModule],
   controllers: [],
   providers: [LandlordObjectsService, LandlordObjectsSerializer, LandlordObjectsControlService],
   exports: [LandlordObjectsService, LandlordObjectsSerializer, LandlordObjectsControlService],
