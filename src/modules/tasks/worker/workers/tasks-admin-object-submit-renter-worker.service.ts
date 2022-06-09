41867ae92c08b8533efc4b4ec4f9157a0bf1c61a
xiagn17 <xiagn17@gmail.com>
2022-06-09 15:19:49 +0200

    FIX: tasks for approving admin

--- src/modules/tasks/worker/workers/tasks-admin-object-submit-renter-worker.service.ts
+++ src/modules/tasks/worker/workers/tasks-admin-object-submit-renter-worker.service.ts
@@ -20,8 +20,8 @@ export class TasksAdminObjectSubmitRenterWorkerService extends TasksQueueBaseSer
     private objectMatchesForLandlordService: ObjectMatchesForLandlordService,
   ) {
     super();
-    this.THROTTLE_MS = 1000;
-    this.THROTTLE_CHUNK_LENGTH = 25;
+    this.THROTTLE_MS = 1500;
+    this.THROTTLE_CHUNK_LENGTH = 10;
 
     this.logger.setContext(this.constructor.name);
   }
